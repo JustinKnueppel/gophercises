16d6168621a6ff1a3ce7cceaac2957f9b727657d
Justin Knueppel <justinknueppel@gmail.com>
2021-08-05 14:36:22 -0400

    Add test for too many entries in record

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -2,6 +2,7 @@ package quiz
 
 import (
 	"encoding/csv"
+	"fmt"
 	"io"
 )
 
@@ -10,10 +11,22 @@ type Question struct {
 	Answer   string
 }
 
+type MarshalQuestionError struct {
+	Line []string
+}
+
+func (e *MarshalQuestionError) Error() string {
+	return fmt.Sprintf("Failed to marshal question from %v", e.Line)
+}
+
 func ReadInput(reader io.Reader) ([]Question, error) {
 	records, err := csv.NewReader(reader).Read()
 	if err != nil {
 		return []Question{}, nil
+	}
+
+	if len(records) > 2 {
+		return []Question{}, &MarshalQuestionError{Line: records}
 	}
 
 	question := Question{
--- quiz/quiz_test.go
+++ quiz/quiz_test.go
@@ -14,22 +14,42 @@ func TestReadInput(t *testing.T) {
 	tests := map[string]struct {
 		reader io.Reader
 		want   []quiz.Question
+		err    error
 	}{
 		"empty": {
 			reader: bytes.NewReader([]byte("")),
 			want:   []quiz.Question{},
+			err:    nil,
 		},
 		"one_line": {
 			reader: bytes.NewReader([]byte("1+1,2")),
 			want:   []quiz.Question{{Question: "1+1", Answer: "2"}},
+			err:    nil,
+		},
+		"one_line_too_many_fields": {
+			reader: bytes.NewReader([]byte("1+1,2,3")),
+			want:   []quiz.Question{},
+			err:    &quiz.MarshalQuestionError{},
 		},
 	}
 
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
 			got, err := quiz.ReadInput(tc.reader)
+
+			if err == nil && tc.err != nil {
+				t.Fatalf("Expected error %t, got nil", tc.err)
+			}
+
 			if err != nil {
-				t.Fatalf("Failed to read input")
+				switch err.(type) {
+				case *quiz.MarshalQuestionError:
+					if _, ok := (tc.err).(*quiz.MarshalQuestionError); !ok {
+						t.Fatalf("Got %v, expected %v", err, tc.err)
+					}
+				default:
+					t.Fatalf("Got unknown error: %v", err)
+				}
 			}
 
 			diff := cmp.Diff(got, tc.want)
