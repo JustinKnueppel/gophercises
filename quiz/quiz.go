f2f0113f6bfabad89f38d90fb4d2a52c289f447e
Justin Knueppel <justinknueppel@gmail.com>
2021-08-05 16:10:48 -0400

    Add comments to exported fields

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -8,11 +8,13 @@ import (
 	"strings"
 )
 
+// Question holds the prompt and answer for a given question
 type Question struct {
 	Question string
 	Answer   string
 }
 
+// MarshQuestionError is used when a given input cannot be marshalled into a Question
 type MarshalQuestionError struct {
 	Line []string
 }
@@ -21,6 +23,7 @@ func (e *MarshalQuestionError) Error() string {
 	return fmt.Sprintf("Failed to marshal question from %v", e.Line)
 }
 
+// ReadInput marshals the input into a slice of Questions
 func ReadInput(reader io.Reader) ([]Question, error) {
 	records, err := csv.NewReader(reader).ReadAll()
 	if err != nil {
@@ -45,10 +48,12 @@ func ReadInput(reader io.Reader) ([]Question, error) {
 	return questions, nil
 }
 
+// AskQuestion prompts the user to answer the given question
 func AskQuestion(writer io.Writer, question Question) {
 	writer.Write([]byte(question.Question + "\n"))
 }
 
+// GetResponse receives input from the user
 func GetResponse(reader *bufio.Reader) (string, error) {
 	text, err := reader.ReadString('\n')
 	if err != nil {
@@ -59,6 +64,7 @@ func GetResponse(reader *bufio.Reader) (string, error) {
 	return text, nil
 }
 
+// CorrectAnswer checks if the user's answer matches the answer to the question
 func CorrectAnswer(response string, question Question) bool {
 	return strings.Compare(response, question.Answer) == 0
 }
